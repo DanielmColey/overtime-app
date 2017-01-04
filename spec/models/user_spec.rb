ea306c3684df343d47a77589a2685a03fe6b3f55
Daniel Coley <dmcoley948562@gmail.com>
2017-01-04 11:02:59 -0700

    created logs and other important things

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,20 +9,30 @@ RSpec.describe User, type: :model do
   	it "can be created" do
   		expect(@user).to be_valid
   	end
+  end
 
-  	it "cannot be created without first_name, last_name" do
-  		@user.first_name = nil
-  		expect(@user).to_not be_valid
-  	end
+    describe "validations" do
+      it "cannot be created without first_name" do
+        @user.first_name = nil
+        expect(@user).to_not be_valid
+    end
 
-    it "cannot be created without first_name, last_name" do
+    it "cannot be created without last_name" do
       @user.last_name = nil
       expect(@user).to_not be_valid
     end
 
-    it "cannot be created without first_name, last_name" do
+    it "cannot be created without phone" do
       @user.phone = nil
       expect(@user).to_not be_valid
+    end
+
+    it 'requires the phone attr to only contain intergers' do
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end
+
+    xit 'requires the phone attr to only have 10 characters' do
     end
   end
 

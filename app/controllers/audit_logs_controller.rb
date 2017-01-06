5593c227224b2d921649d447be78397f2f78cb52
Daniel Coley <dmcoley948562@gmail.com>
2017-01-06 10:15:53 -0700

    refactored the buttons for the mobile app

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -5,9 +5,9 @@ class AuditLogsController < ApplicationController
 	end
 
 	def confirm
-  	audit_log = AuditLog.find(params[:id])
-  	authorize audit_log
-  	audit_log.confirmed!
-  	redirect_to root_path, notice: "Thank you, your confirmation has been successfully made."
-  end
+		audit_log = AuditLog.find(params[:id])
+		authorize audit_log
+		audit_log.confirmed!
+		redirect_to root_path, notice: "Thank you, your confirmation has been successfully made."
+	end
 end
